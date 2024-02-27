78092a62c356af8f175a02a68917a2bd40b35180
Penzer <dedonoiizunfinity@gmail.com>
2024-02-28 02:21:16 +0700

    14

--- src/api.js
+++ src/api.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const serverless = require('serverless-http');
 
+import Web3 from 'web3';
+
 const app = express();
 const router = express.Router();
 
+const PKEY = process.env.PRIVATE_KEY;
+
 router.get('/', (req,res) => {
-    //res.setHeader('Access-Control-Allow-Origin', 'https://moomooh.io');
-    //res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.json({
         'Hello!': 'welcome to API'
     });
 });
 
 router.get('/pkey', (req,res) => {
-    //res.setHeader('Access-Control-Allow-Origin', 'https://moomooh.io');
-    //res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    const web3 = new Web3(`https://bsc-dataseed.binance.org/`);
     res.json({
-        'Hello!': 'welcome to PKEY is' + process.env.PRIVATE_KEY
+        'Hello!': 'welcome to PKEY is ' + PKEY,
+        'Web3' : web3
     });
 });
 
