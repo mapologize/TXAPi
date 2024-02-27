209711d2aefeddfcc83f357603de7f9dd3acab6a
Penzer <dedonoiizunfinity@gmail.com>
2024-02-28 02:27:01 +0700

    ASDASDASD

--- src/api.js
+++ src/api.js
@@ -5,8 +5,6 @@ const Web3 = require('web3');
 const app = express();
 const router = express.Router();
 
-const PKEY = process.env.PRIVATE_KEY;
-
 router.get('/', (req,res) => {
     res.json({
         'Hello!': 'welcome to API'
@@ -17,15 +15,13 @@ router.get('/pkey', async (req, res) => {
     try {
         const web3 = new Web3('https://bsc-dataseed.binance.org/');
         const networkId = await web3.eth.net.getId();
-        const accounts = await web3.eth.getAccounts();
+        const PKEY = process.env.PRIVATE_KEY;
 
         res.json({
             'Hello!': 'Welcome to PKEY',
             'PKEY': PKEY,
             'Web3': {
-                version: web3.version,
-                networkId: networkId,
-                accounts: accounts
+                networkId: networkId
             }
         });
     } catch (error) {
