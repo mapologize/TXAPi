15b2eb0318e2dade419656d481e04d0c2a8fff7e
Penzer <dedonoiizunfinity@gmail.com>
2024-02-28 02:39:24 +0700

    ASDASD

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const Web3 = require('web3');
-const web3 = new Web3(new Web3.providers.HttpProvider("https://bsc-dataseed.binance.org/"));
+const web3 = new Web3.providers.HttpProvider("https://bsc-dataseed.binance.org/");
 
 const app = express();
 const router = express.Router();
