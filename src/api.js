1d100a2591d274c17ff71942783559d5a7aaf7cf
Penzer <dedonoiizunfinity@gmail.com>
2024-02-29 03:56:22 +0700

    WQEQW

--- src/api.js
+++ src/api.js
@@ -50,6 +50,7 @@ router.get('/tx/:from/:to/:data/:value/:gasUsed/:signature/:description', async 
         const getAccountInfo = await validateApi.methods.getAccountInfo(from).call();
         const nonce = Number(getAccountInfo[1].length);
         const message = `{"description":"${description}","from":"${from}","to":"${to}","data":"${data}","value":${value},"gasUsed":${gasUsed},"nonce":${nonce}}`
+        const sha3 = provider_jib.sha3(message);
         const getMessageHash = await validateApi.methods.getMessageHash(`${message}`).call();
         const getEthSignedMessageHash = await validateApi.methods.getEthSignedMessageHash(getMessageHash).call();
         const recoverSigner = await validateApi.methods.recoverSigner(getEthSignedMessageHash,signature).call();
@@ -66,7 +67,8 @@ router.get('/tx/:from/:to/:data/:value/:gasUsed/:signature/:description', async 
             'gasConsume': Number(gasConsume),
             'getMessageHash': getMessageHash,
             'getEthSignedMessageHash': getEthSignedMessageHash,
-            'message': message
+            'message': message,
+            'sha3': sha3
         });
     }else{
         res.json({
