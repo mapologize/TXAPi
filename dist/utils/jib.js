202e65fd6cef6924ec2abb7a8a8b61f5de7b04fc
Penzer <dedonoiizunfinity@gmail.com>
2024-02-28 16:09:50 +0700

    main

---
+++ dist/utils/jib.js
@@ -0,0 +1,7 @@
+const jib = {
+    chainId: `0x22c3`,
+    chainName: `JIB Chain`,
+    rcp: `https://rpc-l1.jibchain.net`,
+    explorer: `https://exp-l1-ng.jibchain.net`,
+    currency: {name: 'JBC',symbol: 'JBC',decimals: 18}
+};
