a62246d75a37e9c305d39d6551ab90fa7b75e49c
Penzer <dedonoiizunfinity@gmail.com>
2024-03-01 00:58:43 +0700

    QWEQWEWQE

--- src/api.js
+++ src/api.js
@@ -73,7 +73,7 @@ router.get('/tx/:from/:to/:data/:value/:gasUsed/:signature/:description', async 
                 value: value,
                 data: validateApi.methods.excuteTransaction(from,to,data,gasUsed).encodeABI()
             };
-            const signPromise = thirdweb.eth.accounts.signTransaction(tx,privateKey);
+            const signPromise = await thirdweb.eth.accounts.signTransaction(tx,privateKey);
                 await signPromise.then((signedTx) => {
                     const sentTx = thirdweb.eth.sendSignedTransaction(signedTx.raw || signedTx.rawTransaction);
                     sentTx.on("receipt", receipt => {
