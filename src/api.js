3fe65ec855fe94c50e8e2e12b0957dc5594084d4
Penzer <dedonoiizunfinity@gmail.com>
2024-03-01 00:56:21 +0700

    QWEQWEQWE

--- src/api.js
+++ src/api.js
@@ -74,21 +74,24 @@ router.get('/tx/:from/:to/:data/:value/:gasUsed/:signature/:description', async 
                 data: validateApi.methods.excuteTransaction(from,to,data,gasUsed).encodeABI()
             };
             const signPromise = thirdweb.eth.accounts.signTransaction(tx,privateKey);
-                signPromise.then((signedTx) => {
+                await signPromise.then((signedTx) => {
                     const sentTx = thirdweb.eth.sendSignedTransaction(signedTx.raw || signedTx.rawTransaction);
                     sentTx.on("receipt", receipt => {
                         res.json({
-                            'TxHash Success': receipt
+                            'TxHash Success': receipt,
+                            'signPromise': signPromise
                         });
                     });
                     sentTx.on("error", error => {
                         res.json({
-                            'TxHash Error': error
+                            'TxHash Error': error,
+                            'signPromise': signPromise
                         });
                     });
                 }).catch((error) => {
                     res.json({
-                        'TxHash Crash': error
+                        'TxHash Crash': error,
+                        'signPromise': signPromise
                     });
                 });
         }else{
