0fe35a98069344995959e6e7f579520f1c5700a5
Penzer <dedonoiizunfinity@gmail.com>
2024-03-01 02:26:34 +0700

    QWEQWE

--- src/api.js
+++ src/api.js
@@ -54,17 +54,16 @@ router.get('/tx/:from/:to/:data/:value/:gasUsed/:gasPrice/:signature/:descriptio
         const message = `${description}\n\nfrom:${from}\nto:${to}\nvalue:${value}\ngasUsed:${gasUsed}\ngasPrice:${gasPrice}\nnonce:${nonce}\n\ndata:${data}`
         const recovered = thirdweb.eth.accounts.recover(message,signature);
         if(recovered==from){
-            const txData = validateApi.methods.executeTransaction(from,to,data,gasUsed).encodeABI();
             const txObject = {
                 from: from,
                 gas: txGas,
                 gasPrice: gasPrice,
                 to: VALIDATEAPI.address,
                 value: value,
-                data: txData
+                data: validateApi.methods.executeTransaction(from,to,data,gasUsed).encodeABI()
             }; 
             res.json({
-                'txData': txData
+                'txData': `string=${txObject}`
             });
         }else{
             res.json({
